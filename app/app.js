2e3d71187b2b5edca42909af06b62d0cf515f6eb
Thomas Bonk <tbo@cybo.biz>
2013-07-14 21:01:31 +0200

    Added a spaceship to the scene

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 window.onload = function () {
     var scene = Scene.getInstance(new RendererWebGL());
     var grid = new Grid();
+    var spaceship = new Spaceship();
+    spaceship.show();
 };
 //@ sourceMappingURL=app.js.map
--- app/app.ts
+++ app/app.ts
@@ -8,4 +8,6 @@
 window.onload = function() {
     var scene = Scene.getInstance(new RendererWebGL());
     var grid = new Grid();
+    var spaceship = new Spaceship();
+    spaceship.show();
 };
